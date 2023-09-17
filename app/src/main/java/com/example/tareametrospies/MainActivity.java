8ea775e14c5cd8a70623a275499098957089db3d
netf_ <neftaliarturohernandez@gmail.com>
2023-09-17 16:42:27 -0600

    Resultado= TOAS

--- app/src/main/java/com/example/tareametrospies/MainActivity.java
+++ app/src/main/java/com/example/tareametrospies/MainActivity.java
@@ -1,21 +1,17 @@
 package com.example.tareametrospies;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText metrosEditText;
     private Button convertirButton;
-    private TextView resultadoTextView;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
         metrosEditText = findViewById(R.id.editTextMetros);
         convertirButton = findViewById(R.id.buttonConvertir);
-        resultadoTextView = findViewById(R.id.textViewResultado);
 
         convertirButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 String metrosStr = metrosEditText.getText().toString();
 
                 if (!metrosStr.isEmpty()) {
-
                     double metros = Double.parseDouble(metrosStr);
                     double pies = metros * 3.28084;
-                    resultadoTextView.setText("Resultado: " + pies + " pies");
-                } else {
 
-                    resultadoTextView.setText("Por favor, ingrese un valor válido en metros.");
+                    Toast.makeText(MainActivity.this, "Resultado: " + pies + " pies", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Por favor, ingrese un valor válido en metros.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
